de6d11e18329ee65a190fc32d88842bea142f9d3
arnistoks <arnis.toks@gmail.com>
2022-07-28 16:17:23 +0300

    feat: add boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import questions from './data/data';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const [chosenAnswer, setChosenAnswer] = useState<number>();
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [result, setResult] = useState(0);
+  const [toggleNextButton, setToggleNextButton] = useState(false);
+  const [toggleAnswer, setToggleAnswer] = useState(false);
+
+  const resultText = () => {
+    if (result < 4) {
+      return 'Man ļoti žēl! Mums nav pa ceļam! :(';
+    } if (result < 7) {
+      return 'Ak vai! Mums būtu jātiekas biežāk!';
+    } if (result < 10) {
+      return 'Apsveicu! Tu pazīsti mani ļoti labi!';
+    }
+    return 'Super! Mēs esam labākie draugi! :)';
+  };
+
+  const resultPercent = () => (result / 10) * 100;
+
+  return (
+    <div className="App">
+      <section className="section">
+        <h1 className="title">Pārbaudi! Cik daudz Tu zini par mani?</h1>
+        {questionNumber !== 10
+          ? (
+            <div className="questionContainer">
+              <h2 className="questionTitle">
+                {questions[questionNumber].question}
+                <span className="questionStep">{`(${questionNumber + 1} no 10)`}</span>
+              </h2>
+              {!toggleAnswer ? (
+                <div className="answersContainer">
+                  {questions[questionNumber].answers.map((answer) => (
+                    <button
+                      key={answer.id}
+                      className="answerButton"
+                      onClick={() => {
+                        setChosenAnswer(answer.id);
+                        setResult(answer.id === questions[questionNumber].correctAnswer ? result + 1 : result);
+                        setToggleNextButton(true);
+                        setToggleAnswer(true);
+                      }}
+                    >
+                      {answer.name}
+                    </button>
+                  ))}
+                </div>
+              )
+                : (
+                  <div className="answersContainer">
+                    {questions[questionNumber].answers.map((answer) => (
+                      <div
+                        key={answer.id}
+                        className={`answerBox ${answer.id === questions[questionNumber].correctAnswer && 'answerCorrect'} ${answer.id === chosenAnswer && chosenAnswer !== questions[questionNumber].correctAnswer && 'answerWrong'}`}
+                      >
+                        {answer.name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+            </div>
+          ) : (
+            <div className="resultContainer">
+              <h2 className="resultTitle">Tavs rezultāts:</h2>
+              <h2 className="resultSummary">{`${resultPercent()}% (${result} no 10)`}</h2>
+              <h2 className="resultRating">{resultText()}</h2>
+            </div>
+          )}
+        { toggleNextButton
+            && (
+            <button
+              className="nextButton"
+              onClick={() => {
+                setToggleNextButton(false);
+                setToggleAnswer(false);
+                setQuestionNumber(questionNumber + 1);
+              }}
+            >
+              {questionNumber !== 9 ? 'Nākošais jautājums' : 'Parādīt rezultātu'}
+            </button>
+            )}
+      </section>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data/data.ts
@@ -0,0 +1,246 @@
+export type Question = {
+    id: number;
+    question: string;
+    answers: Answer[];
+    correctAnswer: number;
+}
+
+type Answer = {
+    id: number;
+    name: string;
+}
+
+const questions: Question[] = [
+  {
+    id: 1,
+    question: 'Kurā datumā man ir dzimšanas diena?',
+    answers: [
+      {
+        id: 1,
+        name: '21. janvārī',
+      },
+      {
+        id: 2,
+        name: '14. februārī',
+      },
+      {
+        id: 3,
+        name: '6. jūnijā',
+      },
+      {
+        id: 4,
+        name: '11. decembrī',
+      },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 2,
+    question: 'Kādā specialitātē es ieguvu bakalaura un maģistra grādu?',
+    answers: [
+      {
+        id: 1,
+        name: 'informācijas tehnoloģijā',
+      },
+      {
+        id: 2,
+        name: 'enerģētikā',
+      },
+      {
+        id: 3,
+        name: 'autotransportā',
+      },
+      {
+        id: 4,
+        name: 'lauksaimniecībā',
+      },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 3,
+    question: 'Kādu pārvietošanās veidu pēdējo 12 mēnešu laikā es izmantoju visbiežāk?',
+    answers: [
+      {
+        id: 1,
+        name: 'sabiedrisko transportu',
+      },
+      {
+        id: 2,
+        name: 'velosipēdu',
+      },
+      {
+        id: 3,
+        name: 'elektrisko skrejriteni',
+      },
+      {
+        id: 4,
+        name: 'automašīnu',
+      },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 4,
+    question: 'Kādi ir mani hobiji?',
+    answers: [
+      {
+        id: 1,
+        name: 'volejbols un dejošana',
+      },
+      {
+        id: 2,
+        name: 'riteņbraukšana un makšķerēšana',
+      },
+      {
+        id: 3,
+        name: 'basketbols un medības',
+      },
+      {
+        id: 4,
+        name: 'hokejs un futbols',
+      },
+    ],
+    correctAnswer: 4,
+  },
+  {
+    id: 5,
+    question: 'Kādu pasākumu es neesmu izlaidis pēdējos 10 gadus pēc kārtas?',
+    answers: [
+      {
+        id: 1,
+        name: 'Pozitivus festivālu',
+      },
+      {
+        id: 2,
+        name: 'Tehnsikās apskates nakti',
+      },
+      {
+        id: 3,
+        name: 'Riteņbraucēju vienības braucienu',
+      },
+      {
+        id: 4,
+        name: 'Rīgas pilsētas svētkus',
+      },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 6,
+    question: 'Kāda bija mana pirmā personīgā automašīna?',
+    answers: [
+      {
+        id: 1,
+        name: 'Opel Astra',
+      },
+      {
+        id: 2,
+        name: 'Volkswagen Golf',
+      },
+      {
+        id: 3,
+        name: 'Audi A2',
+      },
+      {
+        id: 4,
+        name: 'Škoda Octavia',
+      },
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 7,
+    question: 'Kurā valstī es neesmu bijis?',
+    answers: [
+      {
+        id: 1,
+        name: 'Somija',
+      },
+      {
+        id: 2,
+        name: 'Zviedrija',
+      },
+      {
+        id: 3,
+        name: 'Grieķija',
+      },
+      {
+        id: 4,
+        name: 'Īrija',
+      },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 8,
+    question: 'Ar kādu transportlīdzekli pirmo savā mūžā braucu patstāvīgi?',
+    answers: [
+      {
+        id: 1,
+        name: 'mopēdu',
+      },
+      {
+        id: 2,
+        name: 'traktoru',
+      },
+      {
+        id: 3,
+        name: 'automašīnu',
+      },
+      {
+        id: 4,
+        name: 'kvadraciklu',
+      },
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 9,
+    question: 'Kāda ir mana datora marka?',
+    answers: [
+      {
+        id: 1,
+        name: 'Lenovo',
+      },
+      {
+        id: 2,
+        name: 'Hewlett Packard',
+      },
+      {
+        id: 3,
+        name: 'Asus',
+      },
+      {
+        id: 4,
+        name: 'Apple iMac',
+      },
+    ],
+    correctAnswer: 4,
+  },
+  {
+    id: 10,
+    question: 'Kāds transportlīdzeklis man nepieder?',
+    answers: [
+      {
+        id: 1,
+        name: 'traktors',
+      },
+      {
+        id: 2,
+        name: 'piekabe',
+      },
+      {
+        id: 3,
+        name: 'vienritenis',
+      },
+      {
+        id: 4,
+        name: 'motocikls',
+      },
+    ],
+    correctAnswer: 1,
+  },
+];
+
+export default questions;
